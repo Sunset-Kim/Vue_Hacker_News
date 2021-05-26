ff059cec2f709a36dc747dba3d2b01fa3b7607f7
Sunset-Kim <wood2002@naver.com>
2021-05-26 10:26:21 +0900

    axios 적용

--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -1,11 +1,30 @@
 <template>
 	<div>
-		뉴스뷰
+		<div v-for="user in users" v-bind:key="user.id">{{ user.title }}</div>
 	</div>
 </template>
 
 <script>
-export default {};
+import axios from 'axios';
+
+export default {
+	data() {
+		return {
+			users: [],
+		};
+	},
+	created() {
+		let vm = this;
+		axios
+			.get('https://api.hnpwa.com/v0/news/1.json')
+			.then(function(response) {
+				vm.users = response.data;
+			})
+			.catch(function(error) {
+				console.log(error, '비정상적인 접근');
+			});
+	},
+};
 </script>
 
 <style></style>
