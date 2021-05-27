509e3a19a6515cf059a6fe6d454045e6ae5b1ddc
Sunset-Kim <wood2002@naver.com>
2021-05-27 16:58:52 +0900

    ♻️ 유저프로필 공통컴포넌트화 + v-slot 사용

---
+++ src/components/UserProfile.vue
@@ -0,0 +1,49 @@
+<template>
+	<div id="user-info">
+		<div class="user-container">
+			<!-- 아이콘 -->
+			<div class="user-img">
+				<i class="fas fa-user"></i>
+			</div>
+			<!-- 텍스트 -->
+			<div class="user-info">
+				<!-- 아이디 -->
+				<slot name="username"></slot>
+				<div class="user-info-desc">
+					<!-- 아이디정보 -->
+					<slot name="time"></slot>
+					<slot name="karma"></slot>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	// computed: {
+	// 	userInfo() {
+	// 		return this.$store.state.userInfo;
+	// 	},
+	// },
+};
+</script>
+
+<style>
+.user-container {
+	display: inline-flex;
+	padding: 1rem;
+}
+.user-img {
+	display: inline-flex;
+	justify-content: center;
+	align-items: center;
+	margin-right: 2rem;
+
+	font-size: 3rem;
+}
+
+.user-info {
+	color: var(--secondary);
+}
+</style>
