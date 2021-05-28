bd91f1879914e6209355b6e7256184cb035543fa
Sunset-Kim <wood2002@naver.com>
2021-05-28 10:22:32 +0900

    ✨ 스피너 등록

--- src/App.vue
+++ src/App.vue
@@ -6,16 +6,41 @@
 				<router-view class="router-view"></router-view>
 			</transition>
 		</div>
+		<Spinner :loading="loadingStatus"></Spinner>
 	</div>
 </template>
 
 <script>
 import ToolBar from './components/toolBar.vue';
+import Spinner from './components/Spinner.vue';
+import bus from './utils/bus';
 
 export default {
+	created() {
+		bus.$on('start:spinner', this.startSpinner);
+		bus.$on('end:spinner', this.endSpinner);
+	},
+	beforeDestroy() {
+		bus.$off('start:spinner', this.startSpinner);
+		bus.$off('end:spinner', this.endSpinner);
+	},
+	methods: {
+		startSpinner() {
+			this.loadingStatus = true;
+		},
+		endSpinner() {
+			this.loadingStatus = false;
+		},
+	},
+	data() {
+		return {
+			loadingStatus: false,
+		};
+	},
 	name: 'App',
 	components: {
 		ToolBar,
+		Spinner,
 	},
 };
 </script>
